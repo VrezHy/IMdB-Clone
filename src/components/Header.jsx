78a7ce39972ac7fa64ec6654420f05c4cac42b19
VrezHy <dimasiqbal888@gmail.com>
2024-09-23 15:16:50 +0700

    add components file

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs"
+
+export default function Header() {
+  return (
+    <div>
+      <div className="">
+        <MenuItem title="home" address="/" Icon={AiFillHome} />
+        <MenuItem title="about" address="/about" Icon={AiFillHome} />
+      </div>
+      <div className=""></div>
+    </div>
+  );
+}
